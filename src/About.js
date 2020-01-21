5d7d327c166cc26c8e4c63807b67551f2a0efec0
Adrienne Kerr <akerr@lmtc-adkerr.jomax.paholdings.com>
2020-01-21 13:41:21 -0800

    Add about page, favicon

---
+++ src/About.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './css/index.css';
+
+function About() {
+  return (
+    <div className='About'>
+      <div className='info'>
+        <h1>Neon Polygon</h1>
+        <p>
+          Neon Polygon is the music project of Seattle-based synthesist and
+          producer <a href='http://davidhrivnak.com/'>David Hrivnak</a>. Fans of
+          The Chemical Brothers, Boards of Canada and Beck will love Neon
+          Polygon's warm retro electronic sound. Made with psychedelics and
+          love, the music is ripe with vintage synths, acoustic elements, and a
+          full platter of auditory treats for those who listen in.
+        </p>
+        <p>
+          Booking:{' '}
+          <a href='mailto:neonpolygonmusic@gmail.com'>
+            neonpolygonmusic@gmail.com
+          </a>
+        </p>
+        <div style={{ maxWidth: '500px' }}>
+          <iframe
+            title='soundcloud - Neon Polygon'
+            width='100%'
+            height='300'
+            scrolling='no'
+            frameborder='no'
+            allow='autoplay'
+            src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/280528349&color=%2368888c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+          ></iframe>
+        </div>
+        <div className='buzzbtns'>Site by <a href='http://adriennekerr.com'>BUZZBTNS</a></div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './About';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className='App'>
+      <a href='/about' className='kaleidoscope'></a>
+    </div>
+  );
+}
