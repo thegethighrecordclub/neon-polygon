62642a130c2fdcca801288567f8bf9e4a204eff8
Adrienne Kerr <akerr@lmtc-adkerr.jomax.paholdings.com>
2020-01-21 13:47:19 -0800

    add build, cpanel yml

---
+++ build/precache-manifest.5916a6b3c3b7518d4db201ad7591ac52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9686d5df80d5efabfcb2aba7de48f8c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e258fb74a95edd82ea25",
+    "url": "/static/css/main.2ecf39b4.chunk.css"
+  },
+  {
+    "revision": "28fff72065f8f92c6acb",
+    "url": "/static/js/2.bee34798.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.bee34798.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e258fb74a95edd82ea25",
+    "url": "/static/js/main.77832c94.chunk.js"
+  },
+  {
+    "revision": "e4469507823179d012e8",
+    "url": "/static/js/runtime-main.eba7fc2a.js"
+  },
+  {
+    "revision": "173eab63865cda5a0509275aff474f6c",
+    "url": "/static/media/mandala.173eab63.jpg"
+  }
+]);
